98713f97401b4479411a0f8dcf408d77ef5b0b5d
fmalessio <fmalessio@gmail.com>
2021-10-08 14:13:09 -0300

    Starting Email Sender

---
+++ src/main/java/com/fmalessio/alkemy/icons/service/EmailService.java
@@ -0,0 +1,39 @@
+package com.fmalessio.alkemy.icons.service;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import java.io.IOException;
+
+public class EmailService {
+
+    private static String API_KEY = "";
+
+    public static void main(String[] args) throws IOException {
+        Email from = new Email("fmalessio@gmail.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("alessiofederico@hotmail.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(API_KEY); // System.getenv("SENDGRID_API_KEY")
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+
+}
