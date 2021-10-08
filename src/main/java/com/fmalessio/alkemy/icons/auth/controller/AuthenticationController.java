ca4676dbf27e24425bfaa78c6000cb2a39bd25ee
fmalessio <fmalessio@gmail.com>
2021-10-08 12:17:35 -0300

    Get UserDetails from Auth to generate jwt

--- src/main/java/com/fmalessio/alkemy/icons/auth/controller/AuthenticationController.java
+++ src/main/java/com/fmalessio/alkemy/icons/auth/controller/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,24 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    private UserDetailsCustomService userDetailsCustomService;
+    private AuthenticationManager authenticationManager;
     @Autowired
     private JwtUtils jwtTokenUtil;
 
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody AuthenticationRequest authRequest) throws Exception {
+    public ResponseEntity<AuthenticationResponse> singIn(@RequestBody AuthenticationRequest authRequest) throws Exception {
+
+        UserDetails userDetails = null;
         try {
-            this.authenticationManager.authenticate(
+            Authentication auth = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
             );
+            userDetails = (UserDetails) auth.getPrincipal();
         } catch (BadCredentialsException e) {
             throw new Exception("Incorrect username or password", e);
         }
-
-        final UserDetails userDetails = this.userDetailsCustomService
-                .loadUserByUsername(authRequest.getUsername());
 
         final String jwt = jwtTokenUtil.generateToken(userDetails);
 
